cbdd33ee75679e2e52bcd6956a9dd918c91521cb
Punit Arani <punitsai36@gmail.com>
2022-12-13 03:12:39 -0600

    Update all docstrings

--- pkg/random/random.go
+++ pkg/random/random.go
@@ -9,18 +9,30 @@ import (
 // Charset is a set of characters that can be used to generate a random string.
 type Charset string
 
-// Charset Enums
 const (
-	Alpha               Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	Digit               Charset = "0123456789"
-	Special             Charset = "!@#$%^&*()_+-=[]{}|;':\",./<>?"
-	AlphaLower          Charset = "abcdefghijklmnopqrstuvwxyz"
-	AlphaUpper          Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	AlphaNumeric        Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-	AlphaNumericSpecial Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;':\",./<>?"
+	// The Alpha Charset represents the set of uppercase and lowercase letters of the English alphabet.
+	Alpha Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+	// The Digit Charset represents the set of decimal digits.
+	Digit Charset = "0123456789"
+
+	// The Special Charset represents a set of special characters commonly used in passwords.
+	Special Charset = `!@#$%^&*()_+-=[]{}|;':\",./<>?`
+
+	// The AlphaLower Charset represents the set of lowercase letters of the English alphabet.
+	AlphaLower Charset = "abcdefghijklmnopqrstuvwxyz"
+
+	// The AlphaUpper Charset represents the set of uppercase letters of the English alphabet.
+	AlphaUpper Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	// The AlphaNumeric Charset represents the set of uppercase and lowercase letters and decimal digits
+	AlphaNumeric Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	// The AlphaNumericSpecial Charset represents the set of uppercase and lowercase, decimal digits, and special characters.
+	AlphaNumericSpecial Charset = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;':\",./<>?`
 )
 
-// Int64 returns a random int64 number.
+// Int64 generates a random int64 number.
 func Int64() (int64, error) {
 	// Generate a random *big.Int using the crypto/rand package.
 	n, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
@@ -32,10 +44,10 @@ func Int64() (int64, error) {
 	return n.Int64(), nil
 }
 
-// Int returns a random int number.
+// Int generates a random int number within a given range.
 //
-// min is the minimum number.
-// max is the maximum number.
+// param min: The minimum value of the number.
+// param max: The maximum value of the number.
 func Int(min, max int) (int, error) {
 	// Generate a random *big.Int within the given range.
 	n, err := rand.Int(rand.Reader, big.NewInt(int64(max-min+1)))
@@ -47,10 +59,10 @@ func Int(min, max int) (int, error) {
 	return int(n.Int64()) + min, nil
 }
 
-// String returns a random string.
+// String generate a random string with a given length and charset.
 //
-// length is the length of the string.
-// charset is the type of random string.
+// param length: The length of the string.
+// param chars: The characters that can be used to generate the string.
 func String(length int, chars Charset) (string, error) {
 	// Create a slice of bytes to store the generated string.
 	var b []byte
