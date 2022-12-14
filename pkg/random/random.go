7793ca844f8442eaf2ee4360218deb4bd66d7e09
Punit Arani <punitsai36@gmail.com>
2022-12-14 13:58:17 -0600

    Move Int functions to int.go and tests to int_test.go

--- pkg/random/random.go
+++ pkg/random/random.go
@@ -2,7 +2,6 @@ package random
 
 import (
 	"crypto/rand"
-	"math"
 	"math/big"
 )
 
@@ -31,33 +30,6 @@ const (
 	// The AlphaNumericSpecial Charset represents the set of uppercase and lowercase, decimal digits, and special characters.
 	AlphaNumericSpecial Charset = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;':\",./<>?`
 )
-
-// Int64 generates a random int64 number.
-func Int64() (int64, error) {
-	// Generate a random *big.Int using the crypto/rand package.
-	n, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
-	if err != nil {
-		return 0, err
-	}
-
-	// Convert the *big.Int to an int64 and return it.
-	return n.Int64(), nil
-}
-
-// Int generates a random int number within a given range.
-//
-// param min: The minimum value of the number.
-// param max: The maximum value of the number.
-func Int(min, max int) (int, error) {
-	// Generate a random *big.Int within the given range.
-	n, err := rand.Int(rand.Reader, big.NewInt(int64(max-min+1)))
-	if err != nil {
-		return min, err
-	}
-
-	// Add the minimum value to the random *big.Int and return the result as an int.
-	return int(n.Int64()) + min, nil
-}
 
 // String generate a random string with a given length and charset.
 //
--- pkg/random/random_test.go
+++ pkg/random/random_test.go
@@ -1,54 +1,9 @@
 package random
 
 import (
-	"math"
 	"strings"
 	"testing"
 )
-
-// TestInt64 tests the Int64 function.
-func TestInt64(t *testing.T) {
-	// Generate 2**16 random numbers and check if they are within the range.
-	for i := 0; i < 1<<16; i++ {
-		n, err := Int64()
-		if err != nil {
-			t.Error(err)
-		}
-		if n < math.MinInt64 || n > math.MaxInt64 {
-			t.Error("generated number is out of range")
-		}
-	}
-}
-
-// TestInt tests the Int function.
-func TestInt(t *testing.T) {
-	// Generate 2**8 combinations of random numbers and check if they are within the range.
-	for i := 0; i < 1<<8; i++ {
-		// Generate the lower and upper bounds.
-		for l := 0; l < 1<<4; l++ {
-			for w := 0; w < 1<<4; w++ {
-				min := 1 << (1 << l)
-				max := min + (1 << (1 << (w + 1)))
-
-				// Decide the sign of the numbers.
-				for s := 0; s < 2; s++ {
-					if s == 1 {
-						min, max = -max, -min
-					}
-
-					// Generate and test the random number.
-					n, err := Int(min, max)
-					if err != nil {
-						t.Error(err)
-					}
-					if n < min || n > max {
-						t.Error("generated number is out of range")
-					}
-				}
-			}
-		}
-	}
-}
 
 // TestString tests the String function.
 func TestString(t *testing.T) {
