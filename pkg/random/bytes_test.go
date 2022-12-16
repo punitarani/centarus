115168a65127bdb89aaf3b0ddefe4e9addd85c5d
Punit Arani <punitsai36@gmail.com>
2022-12-15 23:50:09 -0600

    Reduce TestBytes max length to 2^12

--- pkg/random/bytes_test.go
+++ pkg/random/bytes_test.go
@@ -6,7 +6,7 @@ func TestBytes(t *testing.T) {
 	// Run each length 2**4 times
 	for i := 0; i < 1<<4; i++ {
 		// Generate the length.
-		for l := 1; l <= 1<<15; l++ {
+		for l := 1; l <= 1<<12; l++ {
 			n, err := Bytes(uint(l))
 			if err != nil {
 				t.Error(err)
