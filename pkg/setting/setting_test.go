ba885ff8237c2a1f0157bc5931da0193bdfce2d6
Punit Arani <punitsai36@gmail.com>
2022-12-23 12:07:38 -0700

    Implement DbCfgParams in Config and add testDbParams()

--- pkg/setting/setting_test.go
+++ pkg/setting/setting_test.go
@@ -37,7 +37,7 @@ func TestLoadConfigFile(t *testing.T) {
 	}
 }
 
-// testDBs checks that the DBs map was populated correctly.
+// testDBs checks that the DbCfg map was populated correctly.
 func testDBs(t *testing.T, dbs map[string]DbCfg) {
 	for dbName, db := range dbs {
 		// Check that the DB name is not empty.
@@ -52,7 +52,10 @@ func testDBs(t *testing.T, dbs map[string]DbCfg) {
 			fieldName := dbVal.Type().Field(i).Name
 			fieldVal := dbVal.Field(i).Interface()
 
-			if fieldVal == "" || fieldVal == nil || fieldVal == 0 {
+			// Handle the Params field.
+			if fieldName == "Params" {
+				testDbParams(t, fieldVal.(DbCfgParams))
+			} else if fieldVal == "" || fieldVal == nil || fieldVal == 0 {
 				// Check that the field value is not empty or 0.
 				t.Errorf("field %v is empty", fieldName)
 			} else if fieldName == "Port" {
@@ -65,6 +68,35 @@ func testDBs(t *testing.T, dbs map[string]DbCfg) {
 				// expectedVal = db_field
 				t.Errorf("field %v is not equal to %v", fieldVal, expectedVal)
 			}
+		}
+	}
+}
+
+// testDbParams checks that the DbCfgParams struct was populated correctly.
+func testDbParams(t *testing.T, params DbCfgParams) {
+	// Check that the Params struct was populated correctly.
+	paramsVal := reflect.ValueOf(params)
+	for i := 0; i < paramsVal.NumField(); i++ {
+		// Get the field name and value.
+		fieldName := paramsVal.Type().Field(i).Name
+		fieldVal := paramsVal.Field(i).Interface()
+		fieldType := paramsVal.Type().Field(i).Type
+
+		if fieldVal == "" || fieldVal == nil || fieldVal == 0 {
+			// Ignore if empty.
+		} else if fieldType == reflect.TypeOf(false) {
+			// Handle bool fields separately.
+			if fieldVal != false {
+				t.Errorf("field %v is not equal to %v", fieldName, false)
+			}
+		} else if fieldType == reflect.TypeOf(1) {
+			// Handle int fields separately.
+			if fieldVal != 1 {
+				t.Errorf("field %v is not equal to %v", fieldName, 1)
+			}
+		} else if expectedVal := fmt.Sprintf("param_%v", fieldName); fieldVal != expectedVal {
+			// Check that the field value is equal to the expected value.
+			t.Errorf("field %v is not equal to %v", fieldVal, expectedVal)
 		}
 	}
 }
