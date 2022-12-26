469488d597c8323da1eafac5552a33c32bef911f
Punit Arani <punitsai36@gmail.com>
2022-12-25 23:42:36 -0700

    Add TransactionFunc type

--- pkg/db/sqlstore/sqlstore.go
+++ pkg/db/sqlstore/sqlstore.go
@@ -7,3 +7,10 @@ type SQLStore struct {
 	Cfg *setting.DbCfg // Database configuration
 	Db  *DbSession     // Database connection
 }
+
+// TransactionFunc is the function signature for database transaction callbacks.
+//
+// param sess: Database session to use for the transaction.
+//
+// return error: Any error that occurred during the transaction.
+type TransactionFunc func(sess *DbSession) error
