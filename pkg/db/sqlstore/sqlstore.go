38ab8050cee4242a2c4f5f5225e97eb1cc53fd67
Punit Arani <punitsai36@gmail.com>
2022-12-25 23:42:16 -0700

    Add SQLStore struct

--- pkg/db/sqlstore/sqlstore.go
+++ pkg/db/sqlstore/sqlstore.go
@@ -1,1 +1,9 @@
 package sqlstore
+
+import "github.com/punitarani/centarus/pkg/setting"
+
+// SQLStore holds the database configuration and connection
+type SQLStore struct {
+	Cfg *setting.DbCfg // Database configuration
+	Db  *DbSession     // Database connection
+}
