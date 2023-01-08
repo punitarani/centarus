5b9758632cb130b00bcded167a244f422c46612d
Punit Arani <punitsai36@gmail.com>
2023-01-07 22:00:52 -0700

    Update GetRoot() to handle only GET requests

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,10 +3,17 @@ package server
 import "net/http"
 
 func GetRoot(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+	// Only handle GET requests
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
+	// Write the response header
+	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 
+	// Write the response body
 	_, err := w.Write([]byte("Centarus"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
